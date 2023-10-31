15cff6bb36537de6ba7bdf3fd5ee8182439d75a8
Evgeniy Kozlov <ek916569@gmail.com>
2023-10-31 19:08:47 +0500

    fff

--- app/utils/getBearerTokenFromCookies.js
+++ app/utils/getBearerTokenFromCookies.js
@@ -3,10 +3,11 @@
 import { useEffect } from "react";
 
 export default function getBearerTokenFromCookies() {
+    let matches = []
 	useEffect(() => {
-		const matches = document.cookie.match(
+		matches = document.cookie.match(
 			new RegExp("(?:^|; )" + "bearer_token".replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, "\\$1") + "=([^;]*)"),
 		);
-		return matches ? decodeURIComponent(matches[1]) : undefined;
 	}, []);
+	return matches ? decodeURIComponent(matches[1]) : undefined;
 }
